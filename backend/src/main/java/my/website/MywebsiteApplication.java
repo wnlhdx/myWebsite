d2241c5200b23e8715f7a2627650eea62d6bae93
wnlhdx <314460007@qq.com>
2021-12-29 21:45:11 +0800

    testPhone2

---
+++ backend/src/main/java/my/website/MywebsiteApplication.java
@@ -0,0 +1,13 @@
+package my.website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MywebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MywebsiteApplication.class, args);
+	}
+
+}
