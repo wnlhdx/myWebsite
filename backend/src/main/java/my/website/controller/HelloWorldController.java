28847b05e2927e45b631a4a8c5c1f99cee0e161d
wnlhdx <314460007@qq.com>
2021-12-29 17:22:29 +0000

    testHelloWorld

---
+++ backend/src/main/java/my/website/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package my.website.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloWorldController {
+    @RequestMapping("/say")
+    public String say(){
+        return "Hello World";
+    }
+}
---
+++ backend/src/test/java/my/controller/HelloWorldControllerTest.java
@@ -0,0 +1,30 @@
+package my.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import my.website.controller.HelloWorldController;
+
+@SpringBootTest
+public class HelloWorldControllerTest {
+    private MockMvc mockMvc;
+    @BeforeEach
+    public void setUp() throws Exception {
+        mockMvc = MockMvcBuilders.standaloneSetup(new HelloWorldController()).build();
+    }
+
+    @Test
+    public void getHello() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/hello/say").accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+    }
+}
